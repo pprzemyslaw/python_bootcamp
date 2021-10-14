fc33300cd5563bcdff149fb9a6f7840fdbd6092d
ppiskorek <ppiskorek@gmail.com>
2021-10-14 19:52:53 +0200

    Nowy program

---
+++ basics.py
@@ -0,0 +1,26 @@
+# podstawa = 10
+# wysokosc = 5
+# poletrojkata = int(podstawa*wysokosc/2)
+# print("Pole trojkata: ", poletrojkata)
+#
+# promienkola = 7
+# polekola = 3.14 * promienkola**2
+# print("Pole kola: ", polekola)
+
+# imie = 'Przemek'
+# wzrost = '185'
+# print(f"Imie: {imie} \nWzrost {wzrost}")
+# liczba = int(input("Podaj imie:"))
+
+#1 Pobierz od uzytkownika imie i wzrost i wypisz w terminalu
+
+imie = input("Podaj Imie:")
+wzrost = input("Podaj Imie:")
+print(f"Imie: {imie} \nWzrost {wzrost}")
+
+
+#2 zapytaj o długość podstawy i wysokosc trojkata i wypisz wyliczone pole
+podstawa = int(input("Podaj podstawe:"))
+wysokosc = int(input("Podaj wysokosc:"))
+poletrojkata = int(podstawa*wysokosc/2)
+print("Pole trojkata: ", poletrojkata)
